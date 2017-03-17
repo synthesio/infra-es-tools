aaa5e5ac7bac475f388443a22249a24d2e605167
Frédéric De Villamil <fdevillamil@synthesio.com>
2017-03-17 11:29:18 +0100

    Code cleaning to use the same constants / checks

--- monitore-threadpools/monitore-threadpools.sh
+++ monitore-threadpools/monitore-threadpools.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+CURL_BIN=$(which curl)
+ENDPOINT=${1}
+THREADPOOL=search
 
-if [ -z $1 ]; then
-  echo "Usage: ${0} endpoint [queue]"
+if [ -z ${1} ]; then
+  echo "Endpoint is missing, exiting"
+  echo "Usage: ${0} some.host.tld [thread pool]"
   echo "Thread pool: bulk, index, percolate, search (default)"
   exit 1
-else
-  endpoint=${1}
 fi
 
-if [ -z $2 ]; then
-  queue=search
-else
-  queue=${2}
+if [ -z ${CURL_BIN} ]; then 
+  echo "Curl binary is missing, exiting" 
+  exit 1 
+fi
+
+if [ ! -z ${2} ]; then
+  THREADPOOL=${2}
 fi
 
 while true; do 
   clear
-  curl -XGET "${endpoint}:9200/_cat/thread_pool?v&h=host,${queue}.queue,${queue}.active,${queue}.rejected,${queue}.completed" 2>/dev/null | sort -unk 2,3 
+  ${CURL_BIN} -XGET "${ENDPOINT}:9200/_cat/thread_pool?v&h=host,${THREADPOOL}.queue,${THREADPOOL}.active,${THREADPOOL}.rejected,${THREADPOOL}.completed" 2>/dev/null | sort -unk 2,3 
   sleep 5 
 done
--- optimize/optimize.sh
+++ optimize/optimize.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
-HOST=$1
+#!/bin/bash
 CURL_BIN=$(which curl)
+ENDPOINT=${1}
 
-if [ -z "$HOST" ]; then
-  echo "Host is missing" 
+if [ -z ${ENDPOINT} ]; then
+  echo "Endpoint is missing, exiting"
+  echo "Usage: ${0} some.host.tld"
   exit 1 
 fi 
 
-if [ -z "$CURL_BIN" ]; then 
-  echo "Curl binary is missing" 
+if [ -z ${CURL_BIN} ]; then 
+  echo "Curl binary is missing, exiting" 
   exit 1 
 fi
 
-for indice in $(${CURL_BIN} -XGET http://${HOST}:9200/_cat/indices | sort -rnk7 | awk '{print $3}'); do 
+for indice in $(${CURL_BIN} -XGET http://${ENDPOINT}:9200/_cat/indices | sort -rnk7 | awk '{print $3}'); do 
   if [ ! -z "$indice" ]; then 
     echo "Processing indice ${indice}" 
-    ${CURL_BIN} -XPOST http://${HOST}:9200/${indice}/_optimize?max_num_segments=1 
+    ${CURL_BIN} -XPOST http://${ENDPOINT}:9200/${indice}/_optimize?max_num_segments=1 
     echo 
   fi 
 done
